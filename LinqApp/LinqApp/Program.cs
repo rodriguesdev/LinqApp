3317c160954bcd147e968d37d296ad92421d46ab
leandro.lacrfghxsh <leandro.lacr@policiacientifica.sp.gov.brcdgh>
2016-10-17 12:56:01 -0200

    SystemDirectory
    Exemplo de Linq to Object

---
+++ LinqApp/LinqApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorio = System.Environment.SystemDirectory;
+            Console.WriteLine(diretorio);
+
+            DirectoryInfo dirInfo = new DirectoryInfo(diretorio);
+
+            var arquivos = dirInfo.GetFiles(".").ToList();
+
+            var agrupado = from a in arquivos
+                           group a by a.Extension into fgroup
+                           orderby fgroup.Key
+                           select fgroup;
+
+            foreach(var filegroup in agrupado)
+            {
+                Console.WriteLine("{0} - {1} arquivo(s)", filegroup.Key, filegroup.Count());
+
+                foreach(var file in filegroup)
+                {
+                    Console.WriteLine(file.Name);
+                }
+
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
